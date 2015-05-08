cc81b60379a988fe104d5151d6f7460f8226bf1f
Michele <mamminim@gmail.com>
2015-05-08 11:54:09 +0200

    developed the correct weather request/response

--- src/APIManager.php
+++ src/APIManager.php
@@ -29,8 +29,6 @@ class APIManager {
 
 
 			$r3->any('/api/weather/', function() use ($db) {
-		
-
 				$body = file_get_contents("php://input");
 
 				$filter=json_decode($body);
@@ -105,7 +103,6 @@ class APIManager {
 				}
 			});
 
-
 			$r3->any('/api/run/*/*', function($id_model, $id_weather_station) use ($db) {
 
 				//fisrt check if weather station exists
@@ -142,54 +139,37 @@ class APIManager {
 				
 				$obj = simplexml_load_string($body);
 				
-				$q = "select * from weather_station where 1=1 ";
-				$var = array();
+				$startTime    = $obj->startTime;
+				$endTime      = $obj->endTime;
+				$toSeparator  = ";";
+				$blSeparator  = ":::";
+				$decSeparator = ".";
 				
-				$ok = true;
-				if( isset($obj->longitude) && isset($obj->latitude) )
+				//print_r($obj);
+				$res_data = $this->getWeatherData($obj);
+				
+				if( $res_data['ok'] )
 					{
-						$q .= " and ST_Distance_Spheroid(geom, st_geomFromText('POINT( '||:lon||' '||:lat||')',4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')<:dist";
-						$q .= " order by ST_Distance_Spheroid(geom, st_geomFromText('POINT( '||:lon||' '||:lat||')',4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]') limit 1";
+						$outcsv = "";
 						
-						$var = array(':lon' => $obj->longitude, ':lat' => $obj->latitude, ':dist' => 50000);
-					}
-				//echo $db->getSQL($q,$var);
-				
-				$res = $db->select($q, $var);
-				if( $res['rowCount'] > 0 )
-					{
-						$ok = true;
-						$q  = "select * from weather_data where id_weather_station = :station";
-						$var = array(':station' => $res['data'][0]['id_weather_station']);
-						//print_r($res['data']);
-						//echo $db->getSQL($q,$var);
-						$res_data = $db->select($q, $var);
-						//print_r($res_data['data']);
-						if( $res_data['rowCount'] > 0 )
+						$aWField = $this->getWeatherParameter($obj);
+						$aWCode  = $this->getWeatherParameter($obj,false);
+
+						for( $r = 0; $r < $res_data['rowCount']; $r++ )
 							{
-								$outcsv = "";
+								$outcsv .= $res_data['data'][$r]['time_ref'] . $toSeparator;
 								
-								for( $r = 0; $r < $res_data['rowCount']; $r++ )
+								for( $nF = 0; $nF < count($aWField); $nF++ )
 									{
-										//echo "|".$res_data['rowCount'][$r]['time_ref']."|";
-										$outcsv .= $res_data['data'][$r]['time_ref'] . ";" . $res_data['data'][$r]['tmin'] . ";" . $res_data['data'][$r]['tmin'] . ":::"; 
+										if( $nF < count($aWField) -1 )
+											$outcsv .= $res_data['data'][$r][$aWField[$nF]] . $toSeparator;
+										else
+											$outcsv .= $res_data['data'][$r][$aWField[$nF]] . $blSeparator;
+										
 									}
+// 								$outcsv .= $res_data['data'][$r]['tmin'] . $toSeparator;
+// 								$outcsv .= $res_data['data'][$r]['tmax'] . $blSeparator; 
 							}
-						else
-							{
-								$ok = false;
-								$msg = "No data available";
-							}
-						
-					}
-				else
-					{
-						$ok = false;
-						$msg = "No station selected";
-					}
-				
-				if( $ok )
-					{
 						
 						$lat="";
 						$lon="";
@@ -202,39 +182,46 @@ class APIManager {
 	
 
 						header('Content-type: application/xml');
-						$xml ='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-									<ns3:WeatherScenarioSimpleResponseMessage xmlns:ns2="http://www.limetri.eu/schemas/ygg" xmlns:ns3="http://www.fispace.eu/domain/ag"> 
-										<latitude>'.$lat.'</latitude>
-										<longitude>'.$lon.'</longitude>
-										<startTime>2015-02-13T00:00:00</startTime>
-										<endTime>2015-02-14T12:00:00</endTime>
-										<weatherVariable>0 0 0</weatherVariable>
-										<weatherVariable>0 1 8</weatherVariable>
-										<blockSeparator>:::</blockSeparator>
-										<decimalSeparator>.</decimalSeparator>
-										<tokenSeparator>;</tokenSeparator>
-										<values>'.$outcsv.'</values>
-									</ns3:WeatherScenarioSimpleResponseMessage>';
-						return $xml;
+						$xml  = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+						$xml .= '			<ns3:WeatherScenarioSimpleResponseMessage xmlns:ns2="http://www.limetri.eu/schemas/ygg" xmlns:ns3="http://www.fispace.eu/domain/ag">';
+						$xml .= '				<latitude>'.$lat.'</latitude>';
+						$xml .= '				<longitude>'.$lon.'</longitude>';
+						$xml .= '				<startTime>'.$startTime.'</startTime>';
+						$xml .= '				<endTime>'.$endTime.'</endTime>';
+						
+						for( $nF = 0; $nF < count($aWCode); $nF++ )
+							{
+								$xml .= '				<weatherVariable>'.$aWCode[$nF].'</weatherVariable>';
+							}
+						$xml .= '				<blockSeparator>'.$blSeparator.'</blockSeparator>';
+						$xml .= '				<decimalSeparator>'.$decSeparator.'</decimalSeparator>';
+						$xml .= '				<tokenSeparator>'.$toSeparator.'</tokenSeparator>';
+						$xml .= '				<values>'.$outcsv.'</values>';
+						$xml .= '			</ns3:WeatherScenarioSimpleResponseMessage>';
 					}
 				else
 					{
 						header('Content-type: application/xml');
-						$xml ='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-									<ns3:WeatherScenarioSimpleResponseMessage xmlns:ns2="http://www.limetri.eu/schemas/ygg" xmlns:ns3="http://www.fispace.eu/domain/ag"> 
-										<latitude>'.$obj->longitude.'</latitude>
-										<longitude>'.$obj->longitude.'</longitude>
-										<startTime>2015-02-13T00:00:00</startTime>
-										<endTime>2015-02-14T12:00:00</endTime>
-										<weatherVariable>0 0 0</weatherVariable>
-										<weatherVariable>0 1 8</weatherVariable>
-										<blockSeparator>:::</blockSeparator>
-										<decimalSeparator>.</decimalSeparator>
-										<tokenSeparator>;</tokenSeparator>
-										<values></values>
-									</ns3:WeatherScenarioSimpleResponseMessage>';
-						return $xml;
+						$xml  = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
+						$xml .= '			<ns3:WeatherScenarioSimpleResponseMessage xmlns:ns2="http://www.limetri.eu/schemas/ygg" xmlns:ns3="http://www.fispace.eu/domain/ag">';
+						$xml .= '				<latitude>'.$lat.'</latitude>';
+						$xml .= '				<longitude>'.$lon.'</longitude>';
+						$xml .= '				<startTime>'.$startTime.'</startTime>';
+						$xml .= '				<endTime>'.$endTime.'</endTime>';
+						
+						for( $nF = 0; $nF < count($aWCode); $nF++ )
+							{
+								$xml .= '				<weatherVariable>'.$aWCode[$nF].'</weatherVariable>';
+							}
+						
+						$xml .= '				<blockSeparator>'.$blSeparator.'</blockSeparator>';
+						$xml .= '				<decimalSeparator>'.$decSeparator.'</decimalSeparator>';
+						$xml .= '				<tokenSeparator>'.$toSeparator.'</tokenSeparator>';
+						$xml .= '				<values></values>';
+						$xml .= '			</ns3:WeatherScenarioSimpleResponseMessage>';
 					}
+				
+				return $xml;
 
 			});
 			
@@ -257,9 +244,100 @@ class APIManager {
 					return '{"ok": false, "msg": "Unvalid request", "url": '.json_encode($url).'}';
 			});
 		}
+	
+	public function getWeatherStations($obj)
+	{
+		$q = "select * from weather_station where 1=1 ";
+		$var = array();
+		
+		if( isset($obj->longitude) && isset($obj->latitude) )
+			{
+				$q .= " and ST_Distance_Spheroid(geom, st_geomFromText('POINT( '||:lon||' '||:lat||')',4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]')<:dist";
+				$q .= " order by ST_Distance_Spheroid(geom, st_geomFromText('POINT( '||:lon||' '||:lat||')',4326),'SPHEROID[\"WGS 84\",6378137,298.257223563]') limit 1";
+				
+				$var = array(':lon' => $obj->longitude, ':lat' => $obj->latitude, ':dist' => 50000);
+			}
+		
+		$res = $this->db->select($q, $var);
+		if( $res['rowCount'] == 0 )
+			$res = array('ok' => false, 'data' => array(), 'message' => "No station selected");
 
+		return $res;
+	}
+
+	
+	public function getWeatherData($obj)
+	{
+		$aWField = $this->getWeatherParameter($obj);
+		
+		$res = $this->getWeatherStations($obj);
+		if( $res['ok'] )
+			{
+				$var = array();
+				$where = "";
+				if( isset($obj->startTime) )
+					{
+						$where .= " and time_ref >= :startTime";
+						$var[':startTime'] = $obj->startTime;
+					}
+				
+				if( isset($obj->endTime) )
+					{
+						$where .= " and time_ref <= :endTime";
+						$var[':endTime'] = $obj->endTime;
+					}
+				
+				$what = "";
+				for( $nF = 0; $nF < count($aWField); $nF++ )
+					{
+						$what .= $aWField[$nF] . ", ";
+					}
+				
+				$q  = "select $what time_ref from weather_data where id_weather_station = :station $where";
+				$var[':station'] = $res['data'][0]['id_weather_station'];
+				$res_data = $this->db->select($q, $var);
+				echo $this->db->getSQL($q,$var);
+				
+				if( $res_data['rowCount'] == 0 )
+					$res_data = array('ok' => false, 'data' => array(), 'message' => "No data available");
+			}
+		else
+			$res_data = $res;
+		
+		return $res_data;
+	}
+	
+	public function getWeatherParameter($obj, $bSolveCode = true)
+	{
+		$nVar = count($obj->weatherVariable);
+		
+		$aWField = array();
+		for( $nV = 0; $nV < $nVar; $nV++ )
+			{
+				$weatherCode = (string) $obj->weatherVariable->$nV;
+				
+				if( $bSolveCode )
+					{
+						switch( $weatherCode )
+							{
+								case '0 0 0': $aWField[] = 'tavg'; break;
+								case '0 0 4': $aWField[] = 'tmin'; break;
+								case '0 0 5': $aWField[] = 'tmax'; break;
+								case '0 1 1': $aWField[] = 'hravg'; break;
+								case '0 1 8': $aWField[] = 'psum'; break;
+								case '0 4 0': $aWField[] = 'rsum'; break;
+								case '0 2 1': $aWField[] = 'wavg'; break;
+							}
+					}
+				else
+					$aWField[] = $weatherCode;
+			}
+		return $aWField;
+		
+	}
 }
 
+	
 
 function run_model($model, $weather){
 
